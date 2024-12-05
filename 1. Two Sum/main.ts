29e81bfe9bb461c2a0950b862788e7322441e092
jakubr1712 <jakubr1712@gmail.com>
2024-12-05 10:36:07 +0100

    twoSum2

--- 1. Two Sum/main.ts
+++ 1. Two Sum/main.ts
@@ -1,17 +1,13 @@
 function twoSum(nums: number[], target: number): number[] | null {
-  for (let i = 0; i <= nums.length; i++) {
-    for (let j = 0; j <= nums.length; j++) {
-      if (i == j) {
-        continue;
-      } else {
-        if (nums[i] + nums[j] === target) {
-          return [i, j];
-          break;
-        }
+  if (nums.length < 2) return null;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
       }
     }
   }
-
   return null;
 }
 
